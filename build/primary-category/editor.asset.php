bb2eb17aafb88b5b51e1ad86d5a17874ea675194
Pamela Ribeiro <contato@pamelanascimento.com>
2026-02-04 07:34:50 -0300

    Remove build content

--- build/primary-category/editor.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-server-side-render'), 'version' => '7f4f2d50df220eb45fdf');
